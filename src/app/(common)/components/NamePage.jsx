d5121200d72d5ff836f98c3d3063b6a9352c39ea
haugse <sara.haugse@gmail.com>
2023-11-13 17:36:33 +0100

    Prøver med useState for å lagre navn hentet fra endepunkt

--- src/app/(common)/components/NamePage.jsx
+++ src/app/(common)/components/NamePage.jsx
@@ -1,31 +1,24 @@
+import {useState} from "react";
 import {Heading} from "@navikt/ds-react";
 
-async function Name() {
-    const personalia = await getPersonalia();
+function Name() {
+     const [name, setName] = useState("");
+
+     fetch('/min-side/api/aduser/api/v1/personalia')
+        .then((response) => {
+            if(response.status === 200) {
+                return response.json();
+            }
+        }).then((data) => {
+                setName(data.navn);
+            }
+        )
 
     return (
         <Heading level="1" size="xlarge" align="center" className="mb-1">
-            {personalia.navn}
+            {name}
         </Heading>
     )
-}
-async function getPersonalia() {
-    const res = await fetch('/min-side/api/aduser/api/v1/personalia', {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-
-    if(res.status === 401) {
-        //login eller feilside
-    } else if(res.status === 404) {
-        // feilside?
-    } else if(!res.ok) {
-
-    }
-    // returnere suksess og json?
-    return res.json();
 }
 
 export default Name;
