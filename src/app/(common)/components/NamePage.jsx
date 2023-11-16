4d3836d24d6017d44dd45ad5805d7dac27dba66d
Ken Ove Andersen <kenove@gmail.com>
2023-11-16 12:42:37 +0100

    Bruk useEffect for henting av personalia

--- src/app/(common)/components/NamePage.jsx
+++ src/app/(common)/components/NamePage.jsx
@@ -1,18 +1,19 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import {Heading} from "@navikt/ds-react";
 
 function Name() {
-     const [name, setName] = useState("");
+    const [name, setName] = useState("");
 
-     fetch('/min-side/api/aduser/api/v1/personalia')
-        .then((response) => {
-            if(response.status === 200) {
-                return response.json();
+    useEffect(() => {
+        async function fetchPersonalia() {
+            const response = await fetch("/min-side/api/aduser/api/v1/personalia")
+            if (response.status === 200) {
+                const json = await response.json();
+                setName(json?.navn);
             }
-        }).then((data) => {
-                setName(data.navn);
-            }
-        )
+        }
+        fetchPersonalia();
+    }, []);
 
     return (
         <Heading level="1" size="xlarge" align="center" className="mb-1">
