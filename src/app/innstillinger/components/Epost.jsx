4b26748fec887190e54ddc8d4a19c8a64ed63443
BAH <bjorn.arne.hagen@nav.no>
2023-11-28 13:56:39 +0100

    deploy:dev

--- src/app/innstillinger/components/Epost.jsx
+++ src/app/innstillinger/components/Epost.jsx
@@ -211,7 +211,6 @@ export default function Epost({ response, harSamtykket, setHarSamtykket, epost, 
                                     </HStack>
                                 </>
                             )}
-
                         </>
             </>
             )}
