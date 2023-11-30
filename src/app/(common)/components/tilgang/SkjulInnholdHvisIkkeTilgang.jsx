fe1d350db475f32db6d7bc3a24f63bde7947e3e2
Ken Ove Andersen <kenove@gmail.com>
2023-11-30 10:07:11 +0100

    Skul innhold for kode 6 brukere

--- src/app/(common)/components/tilgang/SkjulInnholdHvisIkkeTilgang.jsx
+++ src/app/(common)/components/tilgang/SkjulInnholdHvisIkkeTilgang.jsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react";
 import { PersonaliaContext } from "@/app/(common)/components/context/PersonaliaContext";
 import UngIkkeTilgang from "@/app/(common)/components/tilgang/UngIkkeTilgang";
+import Kode6IkkeTilgang from "@/app/(common)/components/tilgang/Kode6IkkeTilgang";
 
 export default function SkulInnholdHvisIkkeTilgang({children}) {
 
@@ -10,6 +11,10 @@ export default function SkulInnholdHvisIkkeTilgang({children}) {
 
     if (personalia.erUnderFemten === true) {
         return <UngIkkeTilgang/>;
+    }
+
+    if (personalia.kanLoggePaa === false) {
+        return <Kode6IkkeTilgang/>;
     }
 
     return <>{children}</>;
