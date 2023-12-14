ebbff263e11c9f21e60d3f2f4d329b4722c8047b
haugse <sara.haugse@gmail.com>
2023-12-14 09:07:32 +0100

    Prøver uten full url

--- src/app/(min-side)/components/MinSidePage.jsx
+++ src/app/(min-side)/components/MinSidePage.jsx
@@ -5,7 +5,6 @@ import { CogIcon } from "@navikt/aksel-icons";
 import Link from "next/link";
 import { useContext } from "react";
 import { PersonaliaContext } from "@/app/(common)/components/context/PersonaliaContext";
-import { ARBEIDSPLASSEN_URL } from "@/app/(common)/utils/constants";
 import LoadingPage from "@/app/(common)/components/LoadingPage";
 import ErrorPage from "@/app/(common)/components/ErrorPage";
 import Feedback from "@/app/(min-side)/components/Feedback";
@@ -42,18 +41,18 @@ export default function MinSidePage() {
 
                     <VStack gap="4" className="mb-14">
                         <HStack gap="4" align="start">
-                            <LinkPanel href={`${ARBEIDSPLASSEN_URL}/stillinger/lagrede-sok`} className="arb-link-panel-primary flex-1">
+                            <LinkPanel href={`/stillinger/lagrede-sok`} className="arb-link-panel-primary flex-1">
                                 <LinkPanel.Title>Mine lagrede søk</LinkPanel.Title>
                                 <LinkPanel.Description>
                                     Bruk et lagret søk for å finne stillinger, eller slett varsel på søk du ikke bruker.
                                 </LinkPanel.Description>
                             </LinkPanel>
-                            <LinkPanel href={`${ARBEIDSPLASSEN_URL}/stillinger/favoritter`} className="arb-link-panel-primary flex-1">
+                            <LinkPanel href={`/stillinger/favoritter`} className="arb-link-panel-primary flex-1">
                                 <LinkPanel.Title>Mine favoritter</LinkPanel.Title>
                                 <LinkPanel.Description>Vis alle annonser du har lagret som favoritter.</LinkPanel.Description>
                             </LinkPanel>
                         </HStack>
-                        <LinkPanel href={`${ARBEIDSPLASSEN_URL}/cv`} className="arb-link-panel-secondary">
+                        <LinkPanel href={`/cv`} className="arb-link-panel-secondary">
                             <LinkPanel.Title>Min CV</LinkPanel.Title>
                             <LinkPanel.Description>
                                 Fyll ut og hold din CV oppdatert for å bruke den ved jobbsøking.
