b8fc0db794804251c24e4da7a10f3806fc21d2a6
BAH <bjorn.arne.hagen@nav.no>
2024-01-26 13:37:32 +0100

    Legger til sidetittel for Samtykker og innstillinger

--- src/app/innstillinger/page.js
+++ src/app/innstillinger/page.js
@@ -1,6 +1,10 @@
 import InnstillingerPage from "@/app/innstillinger/components/InnstillingerPage";
 import {ensureUserLoggedIn} from "@/app/(common)/utils/ensureUserLoggedIn";
 
+export const metadata = {
+    title: "Samtykker og innstillinger - arbeidsplassen.no"
+};
+
 export default async function Page() {
     await ensureUserLoggedIn();
     return <InnstillingerPage />;
