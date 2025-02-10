10f3a775f29dfdf780504a19dea93d0839ffa223
Audun <67591449+audisaudisaudis@users.noreply.github.com>
2025-02-10 10:44:44 +0100

    add umami script in dev (#86)
    * add umami script in dev
    
    * adjust csp headers
    
    * update csp
    
    * update csp
    
    * update csp
    
    * ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  img-src 'self' data: arbeidsplassen.nav.no;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.nav.no;
+  worker-src 'self' blob:;
+  style-src 'self' 'unsafe-inline' nav.no;
+  font-src 'self' nav.no;
+  frame-src 'self' video.qbrick.com;
+  connect-src 'self' amplitude.nav.no umami.nav.no;
+`;
+
+const securityHeaders = [
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+    },
+    {
+        key: "Content-Security-Policy",
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+    },
+];
+
 const nextConfig = {
     basePath: "/min-side",
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: securityHeaders,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
---
+++ src/app/(common)/components/tracking/Umami.js
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+import Script from "next/script";
+
+export default function Umami() {
+    const [isDev, setIsDev] = useState(false);
+
+    useEffect(() => {
+        if (window?.location?.hostname === "arbeidsplassen.intern.dev.nav.no") {
+            setIsDev(true);
+        }
+    }, []);
+
+    if (!isDev) {
+        return null;
+    }
+
+    return (
+        <Script
+            defer
+            src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+            data-host-url="https://umami.nav.no"
+            data-website-id="1cc70e4f-bb41-4d28-8115-cbbc32bee4d3"
+            data-domains="arbeidsplassen.intern.dev.nav.no"
+        />
+    );
+}
